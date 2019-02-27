92e9ca808671adfac8ddbd950315495d3fd8c78a
Matthew Wilson <wilsonmatt219@gmail.com>
2019-02-27 20:23:01 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+42 / 7
 end
 
 def assign_variable(value)
+   "#{value}"
+end
+
+def argue (phrase)
+  "#{phrase}"
   
 end
 
-def argue
-  
-end
-
-def greeting
+def greeting (greeting, name)
+  "#{greeting} , #{name}"
 
 end
 
-def return_a_value
+def return_a_value (string = "Nice")
+  "#{string}"
 
 end
 
 def last_evaluated_value
+  "expert"
 
 end
 
-def pizza_party
+def pizza_party (topping = "cheese")
+  "#{topping}"
 
 end
